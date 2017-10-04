499aa5e57526551a7a62adc16addc23690a589c2
Roman Martinez <ramartinez12@miners.utep.edu>
2017-10-03 21:13:41 -0600

    Added a variable to the getMove call for the Smart strategy

--- play/index.php
+++ play/index.php
@@ -4,50 +4,50 @@ require '../common/Common.php';
 require '../common/Game.php';
 require '../common/Response.php';
 require 'Strategy.php';
-
 $uri = explode('?', $_SERVER['REQUEST_URI']);
+//$uri = explode('?','http://cs3360.cs.utep.edu/ramartinez12//play?pid=59cb50f4b9c23&move=4,4');
 // check is there's a query
 if(count($uri) > 1){
-    // get the pid from the query aka $uri[1]
-    $pid = getParam("pid", $uri[1]);
-    // check if strategy was found
-    if($pid){
-        // get the $move from the query
-        $move = getParam("move", $uri[1]);
-        if($move){
-            $move = explode(',', $move);
-            // convert move to integer array and make the move
-            makeMove($pid, array((int)$move[0], (int)$move[1]));
-        }
-        else{
-            echo json_encode(Response::withReason("Move not specified"));
-        }
-    } else {
-      echo json_encode(Response::withReason("Pid not specified"));  
-    }
+	// get the pid from the query aka $uri[1]
+	$pid = getParam("pid", $uri[1]);
+	// check if strategy was found
+	if($pid){
+		// get the $move from the query
+		$move = getParam("move", $uri[1]);
+		if($move){
+			$move = explode(',', $move);
+			// convert move to integer array and make the move
+			makeMove($pid, array((int)$move[0], (int)$move[1]));
+		}
+		else{
+			echo json_encode(Response::withReason("Move not specified"));
+		}
+	} else {
+		echo json_encode(Response::withReason("Pid not specified"));
+	}
 } else {
-    // $uri did not contain a query
-    echo json_encode(Response::withReason("No pid or move specified"));
+	// $uri did not contain a query
+	echo json_encode(Response::withReason("No pid or move specified"));
 }
-
 function makeMove($pid, $move){
-    // restore the saved game
-    $game = Game::restore($pid);
-    // TODO check is valid move
-    $ackMove = $game->doMove(TRUE, $move);
-    if($ackMove->isWin || $ackMove->isDraw){
-        echo json_encode(Response::withMove($ackMove));        
-    }
-    else {
-        if($game->strategy === "random"){
-            $move = RandomStrategy::getMove(FALSE, $game->board);
-        }
-        else{
-            $move = SmartStrategy::getMove(FALSE, $game->board);
-        }
-        $myMove = $game->doMove(FALSE, $move);
-        echo json_encode(Response::withMoves($ackMove, $myMove));
-    }
-    saveGame($pid, $game);
+	//echo json_encode($move);
+	// restore the saved game
+	$game = Game::restore($pid);
+	// TODO check is valid move
+	$ackMove = $game->doMove(TRUE, $move);
+	if($ackMove->isWin || $ackMove->isDraw){
+		echo json_encode(Response::withMove($ackMove));
+	}
+	else {
+		if($game->strategy === "random"){
+			$move = RandomStrategy::getMove(FALSE, $game->board);
+		}
+		else{
+			$move = SmartStrategy::getMove(FALSE, $game->board, $move);
+		}
+		$myMove = $game->doMove(FALSE, $move);
+		echo json_encode(Response::withMoves($ackMove, $myMove));
+	}
+	saveGame($pid, $game);
 }
 ?>
