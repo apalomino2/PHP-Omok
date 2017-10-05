ddc8da46a805ec74d68dbb3b698401879165c010
Abner Palomino <abner.palomino7@gmail.com>
2017-10-05 11:44:53 -0600

    removed var_dumps

--- play/Strategy.php
+++ play/Strategy.php
@@ -1,12 +1,10 @@
 <?php
 class RandomStrategy{
 	static function getMove($board){
-	    var_dump($board);
 		for(;;){
 			$move[0] = rand(0, 14);
 			$move[1] = rand(0, 14);
 			if(!$board[$move[0]][$move[1]]){
-			    var_dump($move);
 				return $move;
 			}
 		}
