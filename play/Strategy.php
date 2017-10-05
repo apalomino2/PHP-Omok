3dae182ef5219a47559b8dbcb2aaa9a1a7b70637
Roman Martinez <ramartinez12@miners.utep.edu>
2017-10-05 10:47:01 -0600

    Smart strategy returns smart coordinates

--- play/Strategy.php
+++ play/Strategy.php
@@ -10,6 +10,7 @@ class RandomStrategy{
 		}
 	}
 }
+
 class SmartStrategy{
 	static function getMove($bool, $board, $lastMove){
 		
@@ -27,78 +28,95 @@ class SmartStrategy{
 		$temp = 0;
 		$tempValue = array(0,0);	
 		
-		//echo json_encode($tempValue);
+		//echo json_encode($playerMove);
 		
 		//TODO check if there is a win in horizontal of the last move in the board
 		for($i = $startIndexH; $i < $endIndexH; $i++){
+			//echo json_encode($tempValue);
 			if($board[$i][$lastMove[1]] == $playerMove){
 				$counth++;
-			} else if($counth <= $temp){
-				$counth = $temp;
-				if($board[$i][$lastMove[1]] == 0){
+			} else if($counth >= $temp){
+				$temp = $counth;
+				if($temp > 2 && $board[$i][$lastMove[1]] == 0){
+					//echo json_encode($board[$i][$lastMove[1]]);
 					$tempValue[0] = $i;
 					$tempValue[1] = $lastMove[1];
 					break;
+				} else if($temp > 2 && $board[$i][$lastMove[1]] == 2){
+					$tempValue[0] = $i-3;
+					$tempValue[1] = $lastMove[1];
+				} else {
+					$tempValue[0] = rand(0, 14);
+					$tempValue[1] = rand(0, 14);
 				}
 			}
 		}
+		
+		//echo json_encode($tempValue);
 		
 		//TODO checks if there is a win in the vertical of the last move on the board
 		for($i = $startIndexV; $i < $endIndexV; $i++){
+			//echo json_encode($tempValue);
 			if($board[$lastMove[0]][$i] == $playerMove){
-				$counth++;
-			} else if($counth <= $temp){
-						$counth = $temp;
-						if($board[$i][$lastMove[1]] == 0){
+				$countv++;
+			} else if($countv >= $temp){
+						$temp = $countv;
+						if($temp > 2 && $board[$lastMove[0]][$i] == 0){
 							$tempValue[0] = $lastMove[0];
 							$tempValue[1] = $i;
 							break;
+						}  else if($temp > 2 && $board[$i][$lastMove[1]] == 2){
+							$tempValue[0] = $i-3;
+							$tempValue[1] = $lastMove[1];
+						} else {
+							$tempValue[0] = rand(0, 14);
+							$tempValue[1] = rand(0, 14);
 						}
 					}
 				}
-				return array($tempX, $tempY);
-			}
-		
-		/**
-		//TODO checks if there is a win for the both diagonals for the last move
-		for($x = 4; $x < 15; $x++){
-			for($i = 0; $i <= $x; $i++){
-				if($board[$x-$i][$i] == $playerMove){
-					$count1++;
-					if($count1 == 5){
-						return 1;
-					}
-				} else {
-					$count1 = 0;
-				}
-				if($board[14-$x+$i][14-$i] == $playerMove){
-					$count2++;
-					if($count2 == 5){
-						return 1;
-					}
-				} else {
-					$count2 = 0;
-				}
-				
-				if($board[14-$x+$i][$i] == $playerMove){
-					$count3++;
-					if($count3 == 5){
-						return 1;
-					}
-				} else {
-					$count3 = 0;
-				}
-				if($board[$i][14-$x+$i] == $playerMove){
-					$count4++;
-					if($count4 == 5){
-						return 1;
-					}
-				} else {
-					$count4 = 0;
-				}
-			}
-		}
-		**/
-	
+				return $tempValue;
+	}
 }
+/**
+ //TODO checks if there is a win for the both diagonals for the last move
+ for($x = 4; $x < 15; $x++){
+ for($i = 0; $i <= $x; $i++){
+ if($board[$x-$i][$i] == $playerMove){
+ $count1++;
+ if($count1 == 5){
+ return 1;
+ }
+ } else {
+ $count1 = 0;
+ }
+ if($board[14-$x+$i][14-$i] == $playerMove){
+ $count2++;
+ if($count2 == 5){
+ return 1;
+ }
+ } else {
+ $count2 = 0;
+ }
+ 
+ if($board[14-$x+$i][$i] == $playerMove){
+ $count3++;
+ if($count3 == 5){
+ return 1;
+ }
+ } else {
+ $count3 = 0;
+ }
+ if($board[$i][14-$x+$i] == $playerMove){
+ $count4++;
+ if($count4 == 5){
+ return 1;
+ }
+ } else {
+ $count4 = 0;
+ }
+ }
+ }
+ **/
 ?>
+	
+
