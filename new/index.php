885c0c03f352749301b075fa8eef53ee2617cff5
Roman Martinez <ramartinez12@miners.utep.edu>
2017-10-06 00:02:44 -0600

    restore the generator for $pid

--- new/index.php
+++ new/index.php
@@ -1,36 +1,36 @@
 <?php
 // NEW
+// Abner Palomino, Roman Martinez
 require '../common/Common.php';
 require '../common/Game.php';
 require '../common/Response.php';
 /* USE POLYMORPHISM!!! */
-
+//$uri = explode('?', 'http://cs3360.cs.utep.edu/ramartinez12//new?strategy=Smart');
 $uri = explode('?', $_SERVER['REQUEST_URI']);
-// check if there's a query 
+// check if there's a query
 if(count($uri) > 1){
-    // get the strategy from the query aka $uri[1]
-    $strategy = getParam("strategy", $uri[1]);
-    // check if the strategy is valid
-    if($strategy === "smart" || $strategy === "random"){
-        // everything checks out, start a new game
-        newGame($strategy);
-    } elseif ($strategy){
-        // invalid strategy received
-        echo json_encode(Response::withReason("Unknown strategy"));
-    } else {
-        // no strategy found in the query
-        echo json_encode(Response::withReason("Strategy not specified"));
-    }
+	// get the strategy from the query aka $uri[1]
+	$strategy = getParam("strategy", $uri[1]);
+	// check if the strategy is valid
+	if($strategy === "smart" || $strategy === "random"){
+		// everything checks out, start a new game
+		newGame($strategy);
+	} else if ($strategy){
+		// invalid strategy received
+		echo json_encode(Response::withReason("Unknown strategy"));
+	} else {
+		// no strategy found in the query
+		echo json_encode(Response::withReason("Strategy not specified"));
+	}
 } else {
-    // $uri did not contain a query
-    echo json_encode(Response::withReason("Strategy not specified"));
+	// $uri did not contain a query
+	echo json_encode(Response::withReason("Strategy not specified"));
 }
-
 function newGame($strategy){
-    //$pid = uniqid();
-    $pid = "59cb50f4b9c23";
-    $game = new Game($strategy);
-    saveGame($pid, $game);
-    echo json_encode(Response::withPid($pid));
+	 $pid = uniqid();
+	//$pid = "59cb50f4b9c23";
+	$game = new Game($strategy);
+	saveGame($pid, $game);
+	echo json_encode(Response::withPid($pid));
 }
 ?>
